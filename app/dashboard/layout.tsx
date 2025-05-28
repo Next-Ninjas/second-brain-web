e9267a32c07ede26f8b2254a23ed14bb221dff37
jagadishck018 <jagadishck.018@gmail.com>
2025-05-28 10:48:17 +0530

    feat: implement Sidebar and HomeLayout components, and add URL submission functionality in DashboardPage

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,9 +1,8 @@
 import "@/app/globals.css";
-import Sidebar from "./components/Sidebar";
+import Sidebar from "./components/side-bar/Sidebar";
 import { PropsWithChildren } from "react";
 import { TanstackQueryClientProvider } from "@/components/providers/tanstack-query-client-provider";
 import { ThemeProvider } from "@/components/providers/theme-provider";
-import NavigationBar from "@/components/ui/NavigationBar";
 
 export default function HomeLayout({ children }: PropsWithChildren) {
   return (
@@ -17,7 +16,6 @@ export default function HomeLayout({ children }: PropsWithChildren) {
         <div className="flex bg-white dark:bg-black text-gray-900 dark:text-gray-100 min-h-screen">
           <Sidebar />
           <div className="flex flex-col flex-1 min-h-screen overflow-y-auto">
-            <NavigationBar />
             <main className="flex-1">{children}</main>
           </div>
         </div>
